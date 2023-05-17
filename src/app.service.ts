a7f2f90550d1bc6a779933699a9fe882ee68dfa4
d-o0o-b11 <jimin8830@naver.com>
2023-05-17 17:24:04 +0900

    feat >>> op.gg crawling/lol service code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import * as cheerio from 'cheerio';
+// import * as cheerio from 'cheerio';
 import * as puppeteer from 'puppeteer';
 
 @Injectable()
@@ -10,13 +10,12 @@ export class AppService {
   }
 
   async getLolInfo(username: string) {
-    console.log(username);
     const url = `https://www.op.gg/summoner/userName=${username}`;
 
     try {
-      const response = await axios.get(url);
-      const html = response.data;
-      const $ = cheerio.load(html);
+      // const response = await axios.get(url);
+      // const html = response.data;
+      // const $ = cheerio.load(html);
       //----------------------------------------
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
@@ -26,17 +25,46 @@ export class AppService {
       const tier = [];
       const stats = [];
 
-      const nickName = $('h1').text(); //스뿡지밥
+      // const nickName = $('h1').text(); //스뿡지밥
+      const nickName = await page.evaluate(() => {
+        return document.querySelector('.summoner-name').textContent;
+      });
       // const profile_icon = $('.profile-icon img').toString(); //프로필 아이콘
       //<img src=\"https://opgg-static.akamaized.net/meta/images/profile_icons/profileIcon4568.jpg?image=q_auto,f_png,w_auto&amp;v=1684225762498\" alt=\"profile image\">
-      const profile_icon = $('.profile-icon img').attr('src');
+      // const profile_icon = $('.profile-icon img').attr('src');
+      const profile_icon = await page.$eval('.profile-icon img', (e) => e.src);
+
       //https://opgg-static.akamaized.net/meta/images/profile_icons/profileIcon4568.jpg?image=q_auto,f_png,w_auto&v=1684225762498
-      const level = $('.profile-icon span').text(); //레벨
-      // const tier = $('.css-1v663t.e1x14w4w1').toString();
-      const tier_img = $('.css-1v663t.e1x14w4w1 img').attr('src');
-      const tier_tier = $('.css-1v663t.e1x14w4w1 .tier').text();
-      const tier_lp = $('.css-1v663t.e1x14w4w1 .lp').text();
-      const win_lose = $('.css-1v663t.e1x14w4w1 .win-lose-container').text();
+      // const level = $('.profile-icon span').text(); //레벨
+      const level = await page.evaluate(() => {
+        return document.querySelector('.profile-icon span').textContent;
+      });
+
+      const tier_img = await page.$eval(
+        '.css-1v663t.e1x14w4w1 img',
+        (e) => e.src,
+      );
+
+      const tierstore = await page.evaluate(() => {
+        const tier_tier = document.querySelector(
+          '.css-1v663t.e1x14w4w1 .tier',
+        ).textContent;
+
+        const tier_lp = document.querySelector(
+          '.css-1v663t.e1x14w4w1 .lp',
+        ).textContent;
+
+        const win_lose = document.querySelector(
+          '.css-1v663t.e1x14w4w1 .win-lose-container',
+        ).textContent;
+        return { tier_tier, tier_lp, win_lose };
+      });
+
+      // const tier_img = $('.css-1v663t.e1x14w4w1 img').attr('src');
+      // const tier_tier = $('.css-1v663t.e1x14w4w1 .tier').text();
+      // const tier_lp = $('.css-1v663t.e1x14w4w1 .lp').text();
+      // const win_lose = $('.css-1v663t.e1x14w4w1 .win-lose-container').text();
+
       // const recent = $('.css-3i6n1d.ehasqiv3').text(); //<--얘는 안됨
       const stats_win_lose = await page.evaluate(() => {
         return document.querySelector('.css-3i6n1d.ehasqiv3 .stats .win-lose')
@@ -74,7 +102,7 @@ export class AppService {
 
       //<li class=\"css-1qq23jn e1iiyghw3\"> 기준으로 잘라서 배열에 넣기
 
-      tier.push(tier_img, tier_tier, tier_lp, win_lose);
+      tier.push(tier_img, tierstore);
       stats.push(
         stats_win_lose,
         stats_kda,
